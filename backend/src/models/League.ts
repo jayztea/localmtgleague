94d4a74c253ac4126755f92d3b9b2d47a70041e5
JT <jamestmarchant@gmail.com>
2026-07-13 22:11:35 -0400

    Create Services for League Actions

---
+++ backend/src/models/League.ts
@@ -0,0 +1,15 @@
+export interface League {
+
+    league_id: number;
+
+    league_name: string;
+
+    description?: string;
+
+    created_by_user_id: number;
+
+    created_date: Date;
+
+    modified_date: Date;
+
+}
